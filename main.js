0d70241f5e8818c724c73e34af1811f45873b843
ayush <58028488+desibabua@users.noreply.github.com>
2020-06-10 22:25:33 +0530

    added generate_path and bfs function

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-const { bfs } = require('./src/graph.js');
+const { readFileSync } = require('fs');
+const {bfs, generatePaths} = require('./src/graph.js');
 
 const main = function () {
-  
+  const paths = generatePaths(readFileSync, './database/paths.txt', 'utf8');
+  console.log(bfs(paths, 'bb', 'jj'));
+  console.log(bfs(paths, 'jj', 'aa'));
+  console.log(bfs(paths, 'aa', 'hh'));
+  console.log(bfs(paths, 'hh', 'ii'));
+  console.log(bfs(paths, 'ii', 'ee'));
+  console.log(bfs(paths, 'ee', 'mm'));
+  console.log(bfs(paths, 'mm', 'jj'));
 }
 
 main();
--- src/graph.js
+++ src/graph.js
@@ -4,8 +4,41 @@
 // To => to 
 // Should return true.
 
-const bfs = function(pairs,source,target){
-  
+const generatePaths = function (reader,path,encoder) {
+  const paths = reader(path, encoder).trim().split('\n');
+  const separateFromTo = (path) => path.match(/[^"| "]+/g);
+  return Array.from(paths, separateFromTo);
+}
+
+const generateDirectedPairs = function (paths) {
+  return paths.reduce((directedPaths, [from, to]) => {
+    if (from in directedPaths) {
+      directedPaths[from].push(to);
+    } else {
+      directedPaths[from] = [to];
+    }
+    return directedPaths;
+  },{});
+}
+
+const bfs = function (pairs, source, target) {
+  const directedPairs = generateDirectedPairs(pairs);
+
+  const visitedPlaces = [];
+  const queue = [source];
+  const canEnqueued = (place) => !visitedPlaces.includes(place) && !queue.includes(place);
+
+  while (queue.length) {
+    const place = queue.shift();
+    visitedPlaces.push(place);
+    if (place === target) return true;
+    directedPairs[place].forEach(newPlace => {
+      if (canEnqueued(newPlace)) queue.push(newPlace);
+    });
+  }
+
+  return false;
 };
 
-module.exports = {bfs};
+
+module.exports = {bfs, generatePaths};
