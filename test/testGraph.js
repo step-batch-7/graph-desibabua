148b91a0f28b030bbd0d47c5ff442bb0a4a0cea8
ayush <58028488+desibabua@users.noreply.github.com>
2020-06-11 09:21:09 +0530

    added test for bsf

--- test/testGraph.js
+++ test/testGraph.js
@@ -8,4 +8,31 @@ describe('bfs',function() {
     assert.isFalse(bfs(paths,'aa','cc'));
     assert.isTrue(bfs(paths,'aa','bb'));
   });
+
+  it('single node connected to itself', function () {
+    const paths = [ ['aa', 'bb'], ['aa', 'aa'], ];
+    assert.isTrue(bfs(paths, 'aa', 'aa'));
+    assert.isFalse(bfs(paths, 'aa', 'cc'));
+    assert.isTrue(bfs(paths, 'aa', 'bb'));
+  });
+
+  it('two nodes not connected', function () {
+    const paths = [ ['aa', 'bb'], ['cc', 'dd'], ];
+    assert.isFalse(bfs(paths, 'bb', 'bb'));
+    assert.isFalse(bfs(paths, 'aa', 'cc'));
+    assert.isFalse(bfs(paths, 'aa', 'dd'));
+    assert.isFalse(bfs(paths, 'bb', 'cc'));
+    assert.isFalse(bfs(paths, 'cc', 'aa'));
+    assert.isFalse(bfs(paths, 'cc', 'bb'));
+    assert.isTrue(bfs(paths, 'cc', 'dd'));
+    assert.isTrue(bfs(paths, 'aa', 'bb'));
+  });
+
+  it('two nodes perfectly connected', function () {
+    const paths = [ ['aa', 'bb'], ['bb', 'aa'] ];
+    assert.isTrue(bfs(paths, 'bb', 'bb'));
+    assert.isTrue(bfs(paths, 'bb', 'aa'));
+    assert.isTrue(bfs(paths, 'aa', 'aa'));
+    assert.isTrue(bfs(paths, 'aa', 'bb'));
+  });
 });
