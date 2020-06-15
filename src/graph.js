e67a07c46fe058babf2f83cf51e6d80842d73d0b
ayush <58028488+desibabua@users.noreply.github.com>
2020-06-15 21:44:56 +0530

    added dsf

--- src/graph.js
+++ src/graph.js
@@ -1,14 +1,14 @@
-//Example 
+//Example
 // Pairs => [[from,to],[to,from]]
 // Source => from
-// To => to 
+// To => to
 // Should return true.
 
-const generatePaths = function (reader,path,encoder) {
+const generatePaths = function (reader, path, encoder) {
   const paths = reader(path, encoder).trim().split('\n');
   const separateFromTo = (path) => path.match(/[^"| "]+/g);
   return Array.from(paths, separateFromTo);
-}
+};
 
 const generateDirectedPairs = function (paths) {
   return paths.reduce((directedPaths, [from, to]) => {
@@ -17,15 +17,16 @@ const generateDirectedPairs = function (paths) {
     }
     directedPaths[from].push(to);
     return directedPaths;
-  },{});
-}
+  }, {});
+};
 
 const bfs = function (pairs, source, target) {
   const directedPairs = generateDirectedPairs(pairs);
 
   const visitedPlaces = new Set();
   const queue = directedPairs[source] || [];
-  const canEnqueued = (place) => !visitedPlaces.has(place) && !queue.includes(place);
+  const canEnqueued = (place) =>
+    !visitedPlaces.has(place) && !queue.includes(place);
 
   while (queue.length) {
     const place = queue.shift();
@@ -38,5 +39,22 @@ const bfs = function (pairs, source, target) {
   return false;
 };
 
+const search_node = function (directedPairs, source, target, visited) {
+  const child = directedPairs[source] || [];
+  const nonVisitedChild = child.filter((e) => !visited.has(e));
+  visited.add(source);
 
-module.exports = {bfs, generatePaths };
+  if (child.includes(target)) return true;
+  if (nonVisitedChild.length) {
+    return nonVisitedChild.some((e) => search_node(directedPairs, e, target, visited));
+  }
+  return false;
+};
+
+const dfs = function (pairs, source, target) {
+  const directedPairs = generateDirectedPairs(pairs);
+  const visited = new Set();
+  return search_node(directedPairs, source, target, visited);
+};
+
+module.exports = { dfs, bfs, generatePaths };
--- test/testGraph.js
+++ test/testGraph.js
@@ -1,6 +1,6 @@
 const assert = require('chai').assert;
 const sinon = require('sinon');
-const {bfs, generatePaths} = require('../src/graph');
+const {dfs, bfs, generatePaths} = require('../src/graph');
 
 describe('bfs',function() {
   it('single node not connected to itself', function () {
@@ -92,5 +92,112 @@ describe('generateDirectedPairs',function() {
     const expectedValue = [ ['aa', 'bb'], ['aa', 'aa'] ];
     assert.deepStrictEqual(generatePaths(reader, 'temp_path', 'encoder'), expectedValue);
     assert.isTrue(reader.calledOnceWith('temp_path', 'encoder'));
+  });
+});
+
+describe('dfs', function () {
+  it('single node not connected to itself', function () {
+    const paths = [['aa', 'bb']];
+    assert.isFalse(dfs(paths, 'aa', 'aa'));
+    assert.isFalse(dfs(paths, 'aa', 'cc'));
+    assert.isTrue(dfs(paths, 'aa', 'bb'));
+  });
+
+  it('single node connected to itself', function () {
+    const paths = [
+      ['aa', 'bb'],
+      ['aa', 'aa'],
+    ];
+    assert.isTrue(dfs(paths, 'aa', 'aa'));
+    assert.isFalse(dfs(paths, 'aa', 'cc'));
+    assert.isTrue(dfs(paths, 'aa', 'bb'));
+  });
+
+  it('two nodes not connected', function () {
+    const paths = [
+      ['aa', 'bb'],
+      ['cc', 'dd'],
+    ];
+    assert.isFalse(dfs(paths, 'bb', 'bb'));
+    assert.isFalse(dfs(paths, 'aa', 'cc'));
+    assert.isFalse(dfs(paths, 'aa', 'dd'));
+    assert.isFalse(dfs(paths, 'bb', 'cc'));
+    assert.isFalse(dfs(paths, 'cc', 'aa'));
+    assert.isFalse(dfs(paths, 'cc', 'bb'));
+    assert.isTrue(dfs(paths, 'cc', 'dd'));
+    assert.isTrue(dfs(paths, 'aa', 'bb'));
+  });
+
+  it('two nodes perfectly connected', function () {
+    const paths = [
+      ['aa', 'bb'],
+      ['bb', 'aa'],
+    ];
+    assert.isTrue(dfs(paths, 'bb', 'bb'));
+    assert.isTrue(dfs(paths, 'bb', 'aa'));
+    assert.isTrue(dfs(paths, 'aa', 'aa'));
+    assert.isTrue(dfs(paths, 'aa', 'bb'));
+  });
+
+  it('two nodes sparsely connected', function () {
+    const paths = [
+      ['aa', 'bb'],
+      ['bb', 'cc'],
+    ];
+    assert.isTrue(dfs(paths, 'aa', 'bb'));
+    assert.isTrue(dfs(paths, 'bb', 'cc'));
+    assert.isTrue(dfs(paths, 'aa', 'cc'));
+    assert.isFalse(dfs(paths, 'cc', 'bb'));
+    assert.isFalse(dfs(paths, 'cc', 'aa'));
+    assert.isFalse(dfs(paths, 'bb', 'aa'));
+  });
+
+  it('three nodes sparsely connected', function () {
+    const paths = [
+      ['aa', 'bb'],
+      ['bb', 'cc'],
+      ['cc', 'dd'],
+    ];
+    assert.isTrue(dfs(paths, 'aa', 'bb'));
+    assert.isTrue(dfs(paths, 'bb', 'cc'));
+    assert.isTrue(dfs(paths, 'aa', 'cc'));
+    assert.isTrue(dfs(paths, 'aa', 'dd'));
+    assert.isFalse(dfs(paths, 'cc', 'bb'));
+    assert.isFalse(dfs(paths, 'cc', 'aa'));
+    assert.isFalse(dfs(paths, 'bb', 'aa'));
+    assert.isFalse(dfs(paths, 'dd', 'cc'));
+    assert.isFalse(dfs(paths, 'dd', 'aa'));
+  });
+
+  it('three nodes moderately connected', function () {
+    const paths = [
+      ['aa', 'bb'],
+      ['bb', 'cc'],
+      ['cc', 'bb'],
+    ];
+    assert.isTrue(dfs(paths, 'aa', 'bb'));
+    assert.isTrue(dfs(paths, 'bb', 'cc'));
+    assert.isTrue(dfs(paths, 'aa', 'cc'));
+    assert.isTrue(dfs(paths, 'cc', 'bb'));
+    assert.isTrue(dfs(paths, 'cc', 'cc'));
+    assert.isTrue(dfs(paths, 'bb', 'bb'));
+    assert.isFalse(dfs(paths, 'bb', 'aa'));
+    assert.isFalse(dfs(paths, 'aa', 'aa'));
+  });
+
+  it('three nodes perfectly connected', function () {
+    const paths = [
+      ['aa', 'bb'],
+      ['bb', 'cc'],
+      ['cc', 'aa'],
+    ];
+    assert.isTrue(dfs(paths, 'aa', 'bb'));
+    assert.isTrue(dfs(paths, 'bb', 'cc'));
+    assert.isTrue(dfs(paths, 'aa', 'cc'));
+    assert.isTrue(dfs(paths, 'cc', 'bb'));
+    assert.isTrue(dfs(paths, 'cc', 'cc'));
+    assert.isTrue(dfs(paths, 'bb', 'bb'));
+    assert.isTrue(dfs(paths, 'bb', 'aa'));
+    assert.isTrue(dfs(paths, 'aa', 'aa'));
   });
 });
